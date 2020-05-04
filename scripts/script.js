88404d0d4154c84d342b8a67aab0e505c2a57f29
Close-to-Perish <60197975+Close-to-Perish@users.noreply.github.com>
2020-05-04 07:47:30 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -13,11 +13,11 @@ xhr.onload = function() {
 	//for loop throughout content
     for (var i = 0; i < responseObject.pageData.length; i++) {
         newMaterial += '<div class="card">';
-		newMaterial += '<h4>' + responseObject.pageData[i].aheader + '</h4>';
-        newMaterial += '<h3>' + responseObject.pageData[i].bheader + '</h3>';
-		newMaterial += '<h3>' + responseObject.pageData[i].uno + '</h3>';
-		newMaterial += '<p>' + responseObject.pageData[i].uno1 + '</p>';
-		newMaterial += '<ul>';
+		newMaterial += '<h4>' + responseObject.pageData[i].aheader + '</h3>';
+        newMaterial += '<h3>' + responseObject.pageData[i].bheader + '</h4>';
+	        newMaterial += '<ul>';
+		newMaterial += '<li>' + responseObject.pageData[i].uno + '</li>';
+		newMaterial += '<li>' + responseObject.pageData[i].uno1 + '</li>';
 		newMaterial += '<li>' + responseObject.pageData[i].one + '</li>';
 		newMaterial += '<li>' + responseObject.pageData[i].two + '</li>';
 		newMaterial += '<li>' + responseObject.pageData[i].three + '</li>';
